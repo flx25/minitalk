6a852256c05f534f712c0ecbb6f100ef316b3ee9
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-01-30 11:59:29 +0100

    tried embetting a ascii to utf8 conversion

--- sources/client/client.c
+++ sources/client/client.c
@@ -6,21 +6,68 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:46:07 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/30 10:25:13 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/30 11:59:07 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
-int sendstring(int PID, char *str)
+int	utf_len(const char *str)
 {
+	int	len;
+	int	i;
+	int	utf8_len;
 
+	len = ft_strlen(str);
+	i = 0;
+	utf8_len = 0;
+	while (i < len)
+	{
+		if (str[i] < 0x80)
+			utf8_len++;
+		else
+			utf8_len += 2;
+	}
+	return (utf8_len);
+}
+
+unsigned char	*ascii_to_utf8(const char *str)
+{
+	int				j;
+	int				i;
+	unsigned char	*utf8_str;
+	int				len;
+
+	len = ft_strlen(str);
+	utf8_str = malloc(utf_len(str) + 1);
+	if (!utf8_str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < len)
+	{
+		if (str[i] < 0x80)
+			utf8_str[j++] = str[i];
+		else
+		{
+			utf8_str[j++] = 0xC0 | (str[i] >> 6);
+			utf8_str[j++] = 0x80 | (str[i] & 0x3F);
+		}
+		i++;
+	}
+	utf8_str[j] = '\0';
+	return (utf8_str);
+}
+
+int	sendstring(int PID, char *str)
+{
+	ascii_to_utf8();
 }
 
 int	main(int argc, char **argv)
 {
-	int	PID;
+	int	pid;
 
 	if (argc != 3)
 		ft_printf("Please enter the server PID and a string to send!");
@@ -30,10 +77,10 @@ int	main(int argc, char **argv)
 		ft_printf("Please enter a string!");
 	else
 	{
-		PID = ft_atoi(argv[1]);
-		if (PID == 0)
+		pid = ft_atoi(argv[1]);
+		if (pid == 0)
 			ft_printf("Please enter a correct PID!");
 		else
-			sendstring(PID, argv[2]);
+			sendstring(pid, argv[2]);
 	}
 }
