0d9819c9b58733ab7f1b31289c09f938a856849a
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-01-31 15:42:25 +0100

    tried some stuff with server

--- sources/server/server.c
+++ sources/server/server.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:13:09 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/31 11:44:22 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:08:10 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 #include "../../libft/libft.h"
 #include "../../ft_printf/ft_printf.h"
 
+void	handler(int sig)
+{
+	if (sig == SIGUSR1)
+		write(1, "0", 1);
+	if (sig == SIGUSR2)
+		write(1, "1", 1);
+}
+
 int	main(void)
 {
-	ft_printf("The Server PID is %i", (int) getpid());
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	ft_printf("The Server PID is %i \n", (int) getpid());
+	while (1)
+		pause();
 }
