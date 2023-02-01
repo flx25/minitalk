ea47268929792f783a920f214dc4ec6cfcd46561
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-02-01 11:49:45 +0100

    tried something

--- sources/server/server.c
+++ sources/server/server.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:13:09 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/02/01 11:08:26 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/02/01 11:44:02 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,50 @@
 #include "../../libft/libft.h"
 #include "../../ft_printf/ft_printf.h"
 
-void	handler(int sig)
+void	handler(int sig, char *out)
 {
 	if (sig == SIGUSR1)
-		write(1, "0", 1);
+		out = gnl_strjoin(out, "0");
 	if (sig == SIGUSR2)
-		write(1, "1", 1);
+		out = gnl_strjoin(out, "1");
+
+	ft_printf("the binary string is %s", out);
+}
+
+char	*gnl_strjoin(char *str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*nstr;
+
+	if (!str)
+	{
+		str = (char *)malloc(1 * sizeof(char));
+		str[0] = '\0';
+	}
+	if (!str || !buff)
+		return (NULL);
+	nstr = malloc(sizeof(char) * ((ft_strlen(str) + ft_strlen(buff)) + 1));
+	if (nstr == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (str)
+		while (str[++i] != '\0')
+			nstr[i] = str[i];
+	while (buff[j] != '\0')
+		nstr[i++] = buff[j++];
+	nstr[ft_strlen(str) + ft_strlen(buff)] = '\0';
+	free(str);
+	return (nstr);
 }
 
 int	main(void)
 {
 	struct sigaction	sa;
+	char				*out;
 
+	out = ft_calloc(1, sizeof(char));
 	sa.sa_handler = &handler;
 	sigaddset(&sa.sa_mask, SIGUSR1);
 	sigaddset(&sa.sa_mask, SIGUSR2);
