b20e5925e4ed7d98f638a9d31aae58a1dec60b03
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-02-01 10:25:53 +0100

    trying to extablish struct2

---
+++ sources/minitalk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 09:24:43 by fvon-nag          #+#    #+#             */
+/*   Updated: 2023/02/01 10:24:27 by fvon-nag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+
+# undef sa_handler
+
+struct			s_sigaction
+{
+	void		(*my_handler)(int);
+	sigset_t	sa_mask;
+	int			sa_flags;
+};
+
+#endif
--- sources/server/server.c
+++ sources/server/server.c
@@ -6,15 +6,17 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:13:09 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/31 15:08:10 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/02/01 10:25:28 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <signal.h>
+#include "../minitalk.h"
 #include "../../libft/libft.h"
 #include "../../ft_printf/ft_printf.h"
 
-void	handler(int sig)
+void	sa_handler(int sig)
 {
 	if (sig == SIGUSR1)
 		write(1, "0", 1);
@@ -24,8 +26,12 @@ void	handler(int sig)
 
 int	main(void)
 {
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
+	struct s_sigaction	sa;
+
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("The Server PID is %i \n", (int) getpid());
 	while (1)
 		pause();
