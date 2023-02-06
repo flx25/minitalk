ed4c0f74ce8212b9c26e2963e0f3496c5957238a
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-02-06 09:41:10 +0100

    made it more efficient, no char* needed anymore

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:13:09 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/02/02 11:46:20 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/02/06 09:39:38 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,78 +15,29 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
-char			*g_out; // maybe change this one to local
-
-char	*gnl_strjoin(char *str, char *buff)
-{
-	size_t	i;
-	size_t	j;
-	char	*nstr;
-
-	if (!str)
-	{
-		str = (char *)malloc(1 * sizeof(char));
-		str[0] = '\0';
-	}
-	if (!str || !buff)
-		return (NULL);
-	nstr = malloc(sizeof(char) * ((ft_strlen(str) + ft_strlen(buff)) + 1));
-	if (nstr == NULL)
-		return (NULL);
-	i = -1;
-	j = 0;
-	if (str)
-		while (str[++i] != '\0')
-			nstr[i] = str[i];
-	while (buff[j] != '\0')
-		nstr[i++] = buff[j++];
-	nstr[ft_strlen(str) + ft_strlen(buff)] = '\0';
-	free(str);
-	return (nstr);
-}
-
-void	decrypt(void)
-{
-	int		len;
-	int		i;
-	int		j;
-	char	ascii;
-	int		sum;
-
-	len = ft_strlen(g_out);
-	i = 0;
-	while (i < len - 8)
-	{
-		j = i;
-		sum = 0;
-		while (j < i + 8)
-		{
-			sum = (sum << 1) + (g_out[j] - '0');
-			j++;
-		}
-		ascii = (char) sum;
-		write(1, &ascii, 1);
-		i += 8;
-	}
-	ft_memset(g_out, '0', len);
-}
-
 void	handler(int sig)
 {
 	static int	count;
+	static int	sum;
+	char		ascii;
 
 	if (sig == SIGUSR1)
 	{
-		g_out = gnl_strjoin(g_out, "0");
+		sum = (sum << 1) + ('0' - '0');
 		count++;
 	}
 	if (sig == SIGUSR2)
 	{
-			g_out = gnl_strjoin(g_out, "1");
-			count = 0;
+			sum = (sum << 1) + ('1' - '0');
+			count++;
 	}
 	if (count == 8)
-		decrypt();
+	{
+		ascii = (char) sum;
+		write (1, &ascii, 1);
+		count = 0;
+		sum = 0;
+	}
 }
 
 int	main(void)
@@ -99,7 +50,6 @@ int	main(void)
 	sigaddset(&sa.sa_mask, SIGUSR2);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-
 
 	while (1)
 		sleep(0);
